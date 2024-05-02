4c2c69fcb0d377d2449a910f9449d48da3aa8238
Valerii Zarubin <mybigroad@gmail.com>
2024-05-02 17:16:20 +0000

    git commit --date "173 day ago"

---
+++ controllers/openaiControllers.js
@@ -0,0 +1,39 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// generate an image
+const generateImage = async (req, res) => {
+    const { prompt } = request.body
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+          });
+          
+        res.status(200).json({
+            success: true,
+            imageUrl: response.data.data[0].url
+        })
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+
+        res.status(400).json({
+            success: false,
+            message: 'Image could not be generated'
+        })
+    }
+}
+
+module.exports = {
+    generateImage
+}
